8434036b2dc24ce122a96810c6bdd7ee6cb4f9c6
ffd8 <ted@ffd8.org>
2021-02-05 00:10:18 +0100

    updates!
    - updated styling.. fixed canvas height of 50px
    - added a few more algos

--- oscillations.js
+++ oscillations.js
@@ -13,14 +13,16 @@ let osc = {
 	,'round(sin(-x*.1))*10'
 	,'sq(sin(PI/2+x*.1))*10'
 	,'sq(sin(x*.1))*10'
+	,'sin(x*.1)*15+sin(x*.5)*5'
+	,'x*sin(x*.1)*15%20'
 	],
 
 	misc : [
-	'x%10+2'
-	,'-x%10+10'
+	'x%20'
+	,'-x%20'
 	,'log(x)*3'
 	,'random(x*.1)'
 	,'noise(x*.1)*h-h/2'
-	,'tan(x*h)'
+	,'tan(x*20)'
 	],
 }
